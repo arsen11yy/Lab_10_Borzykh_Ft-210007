66974847a25ef633da0c4f8440dce73382068a1c
#Student(/arsen11y\) <112753125+arsen11yy@users.noreply.github.com>
2022-11-30 02:47:46 +0500

    Update Programm.py

--- Programm.py
+++ Programm.py
@@ -1,1 +1,10 @@
-class Programm
+class Programm:
+  n = int(input("Введите натуральное число N = "))
+  d = 64
+  while n > 0:
+    k = n // d
+    if k > 0:
+        print("{0} - {1}".format(d, k))
+        n = n % d
+    d = d // 2
+
